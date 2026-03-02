c8529540ee60cb048b6a119c7a572f2838555b64
youyouxusg <youyou.xu.2025@pbs.smu.edu.sg>
2026-03-02 11:47:48 +0800

    Add example_load_data2.py

---
+++ example_load_data2.py
@@ -0,0 +1,12 @@
+import pmxt
+
+api = pmxt.Exchange(exchange_name='Polymarket')
+
+# 1. Search for the broad Event
+events = api.fetch_events(query='Who will Trump nominate as Fed Chair?')
+fed_event = events[0]
+
+# 2. Find the specific Market within that event
+warsh = fed_event.markets.match('Kevin Warsh')
+
+print(f"Price: {warsh.yes.price}")
